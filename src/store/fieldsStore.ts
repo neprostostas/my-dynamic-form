8618a996f8e1d30323ae0cc806584f44e62ac981
neprostostas <neprostostas.gg@gmail.com>
2024-06-08 14:58:23 +0300

    store divided in separate modules

--- src/store/fieldsStore.ts
+++ src/store/fieldsStore.ts
@@ -1,58 +1,20 @@
 import { defineStore } from "pinia";
-import { ref } from "vue";
-import { Field } from "@/types";
+import {
+  fields,
+  addField,
+  removeField,
+  updateVowelCount,
+} from "@/store/modules/fields";
+import {
+  searchQuery,
+  searchQueryMatches,
+  handleInputChange,
+  highlightMatchingFields,
+} from "@/store/modules/search";
 
 export const useFieldsStore = defineStore(
   "fieldsStore",
   () => {
-    const fields = ref<Field[]>([]);
-    const searchQuery = ref<string>("");
-    const searchQueryMatches = ref<boolean>(false);
-
-    const addField = () => {
-      if (fields.value.length >= 10) {
-        return;
-      }
-      fields.value.push({
-        id: Date.now(),
-        value: "",
-        vowelCount: 0,
-        highlighted: false,
-      });
-    };
-
-    const removeField = (index: number) => {
-      if (fields.value.length <= 1) {
-        return;
-      }
-      fields.value.splice(index, 1);
-    };
-
-    const handleInputChange = (index: number) => {
-      updateVowelCount(fields.value[index]);
-      highlightMatchingFields();
-    };
-
-    const updateVowelCount = (field: Field) => {
-      const value = field.value;
-      const vowelMatch = value.match(/[aeiou]/gi);
-      field.vowelCount = vowelMatch ? vowelMatch.length : 0;
-    };
-
-    const highlightMatchingFields = () => {
-      const query = searchQuery.value.toLowerCase();
-      let matchFound = false;
-      fields.value.forEach((field) => {
-        if (query && field.value.toLowerCase().includes(query)) {
-          field.highlighted = true;
-          matchFound = true;
-        } else {
-          field.highlighted = false;
-        }
-      });
-      searchQueryMatches.value = matchFound;
-    };
-
     return {
       fields,
       searchQuery,
