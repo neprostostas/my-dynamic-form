e14d95f3eced07d3b0245148ca1cf168779abed2
neprostostas <neprostostas.gg@gmail.com>
2024-06-08 15:20:37 +0300

    Revert "divided in components"
    This reverts commit 179f2d8e

--- src/components/DynamicForm.vue
+++ src/components/DynamicForm.vue
@@ -1,64 +1,67 @@
 <template>
   <div class="form-container">
-    <SearchInput
-      v-bind="{ searchQuery, searchQueryMatches }"
-      @updateSearchQuery="updateSearchQuery"
+    <input
+      v-model="searchQuery"
+      placeholder="Search"
+      @input="highlightMatchingFields"
+      :class="{ highlight: searchQueryMatches }"
+      class="input-field search"
     />
 
-    <FieldInput
+    <div
       v-for="(field, index) in fields"
       :key="field.id"
-      v-bind="{ index, field }"
-      @updateFieldValue="updateFieldValue"
-      @removeField="removeField"
-    />
+      class="form-field"
+      :class="{ highlight: field.highlighted }"
+    >
+      <input
+        v-model="field.value"
+        :placeholder="'Field ' + (index + 1)"
+        @input="() => handleInputChange(index)"
+        class="input-field"
+      />
+      <button @click="() => removeField(index)" class="btn btn-remove">
+        Remove
+      </button>
+      <span>Vowels: {{ field.vowelCount }}</span>
+    </div>
 
-    <Button
+    <button
       @click="addField"
-      buttonClass="btn-add"
+      class="btn btn-add"
       :disabled="fields.length >= 10"
     >
       Add Field
-    </Button>
+    </button>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { storeToRefs } from "pinia";
 import { useFieldsStore } from "@/store/fieldsStore";
-import FieldInput from "@/components/FieldInput.vue";
-import SearchInput from "@/components/SearchInput.vue";
-import Button from "@/components/Button.vue";
 
 const fieldsStore = useFieldsStore();
 const { fields, searchQuery, searchQueryMatches } = storeToRefs(fieldsStore);
-const {
-  addField,
-  removeField: removeFieldFromStore,
-  handleInputChange,
-  highlightMatchingFields,
-} = fieldsStore;
-
-const updateFieldValue = (index: number, value: string) => {
-  fields.value[index].value = value;
-  handleInputChange(index);
-};
-
-const updateSearchQuery = (query: string) => {
-  searchQuery.value = query;
-  highlightMatchingFields();
-};
-
-const removeField = (index: number) => {
-  removeFieldFromStore(index);
-};
+const { addField, removeField, handleInputChange, highlightMatchingFields } =
+  fieldsStore;
 </script>
 
-<style>
+<style scoped>
 .form-container {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.form-field {
+  border-radius: 15px;
+  padding: 10px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 20px;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 10px;
+  width: 400px;
 }
 .input-field {
   flex: 1;
@@ -66,6 +69,30 @@ const removeField = (index: number) => {
   border: 1px solid #ccc;
   border-radius: 4px;
   transition: background-color 0.3s ease;
+}
+.input-field.search {
+  width: 380px;
+  margin: 20px 0 30px;
+}
+.btn {
+  padding: 10px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+.btn-add {
+  margin-top: 20px;
+  background-color: #007bff;
+  color: white;
+}
+.btn-remove {
+  background-color: #dc3545;
+  color: white;
+}
+.btn:disabled {
+  background-color: #ccc;
+  cursor: not-allowed;
 }
 .highlight {
   transition: background-color 0.3s ease;
